bac8888270119cdaf9f7c5af40d77febcd6ff268
Zinda123 <76554934+Zinda123@users.noreply.github.com>
2020-12-25 07:20:13 +0500

    Create Mujeeb.py

---
+++ Mujeeb.py
@@ -0,0 +1,13 @@
+Salam malgaro snga yai
+umeed laram tool ba da sehat pa jama ki yai
+da commond ghalat use na kii ha
+shahbaz khan 50gb hhh
+nehad khan chata waya ma
+Ayash larka warak e na hkare
+
+la yarano zaar sham 
+sta da mah pa tamasha ym 
+aw la ta jaar sham 
+pa aman mar yam 
+lka da marr 
+jarr peri
